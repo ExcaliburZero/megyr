e5d740f24e2a6fe9ac40a62486317e569b423d15
Christopher Wells <cwellsny@nycap.rr.com>
2018-09-10 19:37:27 -0400

    Add initial handling of config and params files

--- megyr/__main__.py
+++ megyr/__main__.py
@@ -1,5 +1,55 @@
+import argparse
+import json
+import os.path
+import sys
+
 def main():
-    print("Hello, World")
+    args = process_args(sys.argv)
+
+    validate_args(args)
+
+    with open(args.config_file, "r") as config_f:
+        with open(args.params_file, "r") as params_f:
+            config = json.load(config_f)
+            validate_config(config)
+
+            params = json.load(params_f)
+            validate_params(params)
+
+            megyr(config, params, args.work_dir)
+
+def create_arg_parser():
+    parser = argparse.ArgumentParser(description="")
+
+    parser.add_argument("config_file", type=str, help="")
+    parser.add_argument("params_file", type=str, help="")
+    parser.add_argument("work_dir", type=str, help="")
+
+    return parser
+
+def process_args(argv):
+    parser = create_arg_parser()
+
+    return parser.parse_args(argv[1:])
+
+def validate_args(args):
+    assert(os.path.isfile(args.config_file))
+    assert(os.path.isfile(args.params_file))
+    assert(os.path.isdir(args.work_dir))
+
+def validate_config(config):
+    assert("output_dir" in config)
+    assert("mesa_configs" in config)
+    assert("gyre_configs" in config)
+
+def validate_params(params):
+    assert("mesa" in params)
+    assert("gyre" in params)
+
+def megyr(config, params, work_dir):
+    print(config)
+    print(params)
+    print(work_dir)
 
 if __name__ == "__main__":
     main()
