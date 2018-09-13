83f37823259535666167cd705a77478d4f8faaa4
Christopher Wells <cwellsny@nycap.rr.com>
2018-09-13 10:50:53 -0400

    Write prfoile summary files and use them for caching

--- megyr/__main__.py
+++ megyr/__main__.py
@@ -3,6 +3,8 @@ import json
 import os.path
 import sys
 
+import pandas as pd
+
 import gyre
 import mesa
 import parameters
@@ -46,6 +48,7 @@ def validate_config(config):
     assert("output_dir" in config)
     assert("mesa_configs" in config)
     assert("gyre_config" in config)
+    assert("mesa_profiles_summary_filename" in config)
     assert("star_exec_location" in config)
     assert("gyre_location" in config)
     assert("mesa_MP_threads" in config)
@@ -69,7 +72,7 @@ def megyr(config, params, work_dir):
 
         mesa_dir_name, logs_dir_name = mesa.run_mesa(config, comb, work_dir, output_dir)
 
-        values, rows = mesa.get_mesa_data(config, output_dir, mesa_dir_name, logs_dir_name)
+        values, rows = load_or_collect_mesa_data(config, output_dir, mesa_dir_name, logs_dir_name)
 
         gyre_grid = parameters.create_grid(values, rows, params["gyre"])
 
@@ -78,6 +81,18 @@ def megyr(config, params, work_dir):
         for gyre_comb in gyre_grid:
             print("GYRE: " + str(gyre_comb))
             gyre.run_gyre(config, values, rows, comb, gyre_comb, work_dir, output_dir, mesa_dir_name, logs_dir_name)
+
+def load_or_collect_mesa_data(config, output_dir, mesa_dir_name, logs_dir_name):
+    profiles_summary_file = os.path.join(output_dir, mesa_dir_name, config["mesa_profiles_summary_filename"])
+    if os.path.isfile(profiles_summary_file):
+        values = {}
+        rows = pd.read_csv(profiles_summary_file)
+    else:
+        values, rows = mesa.get_mesa_data(config, output_dir, mesa_dir_name, logs_dir_name)
+
+        rows.to_csv(profiles_summary_file, index=False)
+
+    return values, rows
 
 if __name__ == "__main__":
     main()
